b33872bb30d5db6d2a8c92195db62019cf4be62c
idzhalalov <idzhalalov@gmail.com>
2018-11-09 18:42:50 +0500

    Limitations are added

--- lib.php
+++ lib.php
@@ -1,15 +1,24 @@
 <?php
 declare(strict_types=1);
 
+const BASE = 2;
+const EXP = 10;
+const MAX_MEMBERS = BASE * (EXP ** 5);
+const MAX_SCORES = EXP ** 9;
+
 /**
  * Get ranks of all games
  *
- * @param int $membersCount The game members count
- * @param string $scores Total scores of the game
- * @param $aliceGamesCount Total games of Alice
- * @param $aliceScores Scores of Alice
+ * @param int    $membersCount    The game members count
+ * @param string $scores          Total scores of the game
+ * @param int    $aliceGamesCount Total games of Alice
+ * @param array  $aliceScores     Scores of Alice
  *
  * @return array Ranks of games
+ *
+ * @throws Exception When one of limits is exceeded
+ * ($membersCount and $aliceGamesCount are limited with MAX_MEMBERS constant;
+ * and any score is limited by MAX_SCORES)
  */
 function ranks(
     int $membersCount,
@@ -19,6 +28,29 @@ function ranks(
 ): array {
     $result = [];
     $scoresArray = explode(' ', str_replace('  ', ' ', $scores));
+
+    // Check for limitations
+    if ($membersCount > MAX_MEMBERS) {
+        throw new Exception('Too many members. Max members count is '
+            .MAX_MEMBERS);
+    }
+    if ($aliceGamesCount > MAX_MEMBERS) {
+        throw new Exception('Too many games count. Max games count is '
+            .MAX_MEMBERS);
+    }
+    array_walk($scoresArray, function ($score) {
+        if ($score > MAX_SCORES) {
+            throw new Exception('Too large value of a score '.$score
+                .'. Max possible score is '.MAX_SCORES);
+        }
+    });
+    array_walk($aliceScores, function ($score) {
+        if ($score > MAX_SCORES) {
+            throw new Exception('Alice has too large value of a score '.$score
+                .'. Max possible score is '.MAX_SCORES);
+        }
+    });
+
     foreach ($aliceScores as $score) {
         $result[] = rankOfGame($score, $scoresArray);
     }
@@ -29,8 +61,8 @@ function ranks(
 /**
  * Get rank of current game
  *
- * @param int $scoreOfGame Scores of a game
- * @param array $allScores All scores
+ * @param int   $scoreOfGame Scores of a game
+ * @param array $allScores   All scores
  *
  * @return int Rank of the game
  */
--- tests/RanksTest.php
+++ tests/RanksTest.php
@@ -7,6 +7,9 @@ use PHPUnit\Framework\TestCase;
 
 class RanksTest extends TestCase
 {
+    private $invalidMax = 2 * 10 ** 5 + 1;
+    private $invalidMaxScore = 2 * 10 ** 9 + 1;
+
     public function testRanks()
     {
         $ranks = ranks(7, '100 100 50 40 40 20 10', 4, [5, 25, 50, 120]);
@@ -19,5 +22,49 @@ class RanksTest extends TestCase
         $this->assertEquals(4, rankOfGame(25, [100, 100, 50, 40, 40, 20, 10]));
         $this->assertEquals(2, rankOfGame(50, [100, 100, 50, 40, 40, 20, 10]));
         $this->assertEquals(1, rankOfGame(120, [100, 100, 50, 40, 40, 20, 10]));
+    }
+
+    public function testMaxMembers()
+    {
+        $this->expectException(Exception::class);
+        ranks(
+            $this->invalidMax,
+            '100 100 50 40 40 20 10',
+            4,
+            [5, 25, 50, 120]
+        );
+    }
+
+    public function testMaxGames()
+    {
+        $this->expectException(Exception::class);
+        ranks(
+            7,
+            '100 100 50 40 40 20 10',
+            $this->invalidMax,
+            [5, 25, 50, 120]
+        );
+    }
+
+    public function testMaxPossibleScore()
+    {
+        $this->expectException(Exception::class);
+        ranks(
+            7,
+            '100 100 50 40 40 20 10 ' . $this->invalidMaxScore,
+            4,
+            [5, 25, 50, 120]
+        );
+    }
+
+    public function testMaxPossibleScoreOfAlice()
+    {
+        $this->expectException(Exception::class);
+        ranks(
+            7,
+            '100 100 50 40 40 20 10 ',
+            4,
+            [5, 25, 50, $this->invalidMaxScore]
+        );
     }
 }
