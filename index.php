857cb2dd7bf3f8255f9dc9681093f415c6c3ed95
idzhalalov <idzhalalov@gmail.com>
2018-11-09 18:30:11 +0500

    Comments are added

--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 declare(strict_types=1);
 require_once 'lib.php';
 
+// Ranks of all previous games
 $ranks = ranks(7, '100 100 50 40 40 20 10', 4, [5, 25, 50, 120]);
 foreach ($ranks as $rank) {
     echo $rank . "<br>";
 }
+
+// Updated rank
+echo rankOfGame(120, [125, 125, 120, 120, 100, 100, 50, 40, 40, 20, 10]);
--- lib.php
+++ lib.php
@@ -1,6 +1,16 @@
 <?php
 declare(strict_types=1);
 
+/**
+ * Get ranks of all games
+ *
+ * @param int $membersCount The game members count
+ * @param string $scores Total scores of the game
+ * @param $aliceGamesCount Total games of Alice
+ * @param $aliceScores Scores of Alice
+ *
+ * @return array Ranks of games
+ */
 function ranks(
     int $membersCount,
     string $scores,
@@ -16,6 +26,14 @@ function ranks(
     return $result;
 }
 
+/**
+ * Get rank of current game
+ *
+ * @param int $scoreOfGame Scores of a game
+ * @param array $allScores All scores
+ *
+ * @return int Rank of the game
+ */
 function rankOfGame(int $scoreOfGame, array $allScores)
 {
     array_push($allScores, $scoreOfGame);
